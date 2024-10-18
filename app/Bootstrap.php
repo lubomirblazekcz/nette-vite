858573c1e5c1077bad91bb8d508ee6b0d90bf6b6
Lubomír Blažek <lubomir.blazek@newlogic.cz>
2024-10-18 23:36:07 +0200

    feat: added debug mode

--- app/Bootstrap.php
+++ app/Bootstrap.php
@@ -8,23 +8,24 @@ use Nette\Bootstrap\Configurator;
 
 class Bootstrap
 {
-	public static function boot(): Configurator
-	{
-		$configurator = new Configurator;
+    public static function boot(): Configurator
+    {
+        $configurator = new Configurator;
         $rootDir = dirname(__DIR__);
+        $debug = getenv('NETTE_DEBUG');
 
-		$configurator->setDebugMode(true);
-		$configurator->enableTracy($rootDir . '/log');
+        $configurator->setDebugMode($debug && strtolower($debug) === 'true' ?: $debug);
+        $configurator->enableTracy($rootDir . '/log');
 
-		$configurator->setTempDirectory($rootDir . '/temp');
+        $configurator->setTempDirectory($rootDir . '/temp');
 
-		$configurator->createRobotLoader()
-			->addDirectory(__DIR__)
-			->register();
+        $configurator->createRobotLoader()
+            ->addDirectory(__DIR__)
+            ->register();
 
-		$configurator->addConfig($rootDir . '/config/common.neon');
-		$configurator->addConfig($rootDir . '/config/services.neon');
+        $configurator->addConfig($rootDir . '/config/common.neon');
+        $configurator->addConfig($rootDir . '/config/services.neon');
 
-		return $configurator;
-	}
+        return $configurator;
+    }
 }
